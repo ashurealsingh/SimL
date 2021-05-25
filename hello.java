674d9136c4bb5cea9542ebda1f171dc793e1c61f
ashurealsingh <ashu.realsingh@gmail.com>
2021-05-25 07:51:21 +0000

    using cherry pick in right branch4

--- hello.java
+++ hello.java
@@ -1,7 +1,17 @@
 class plue{
 public static void main()
 {
+<<<<<<< HEAD
+=======
+int a(10);
+int b=20;
+int c = a()+b;
+>>>>>>> 252c00a... hello to the new cherry pick
 System.out.println("hello world");
 //This is out new program in activity branch
+}
+int a(){
+int a,b;
+return a;
 }
 }
