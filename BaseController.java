2c55857f2cac78afd5ad3753052b6618a329561d
ashurealsingh <ashu.realsingh@gmail.com>
2021-05-27 07:27:27 +0000

    cherrypick

--- BaseController.java
+++ BaseController.java
@@ -38,17 +38,3 @@ public class BaseController {
 	}
 
 }
-// Loads the standardBuild function/step from workflowLibs.git/vars/standardBuild.groovy
-// and invokes it.
-standardBuild {
-    environment = 'golang:1.5.0'
-    mainScript = '''
-go version
-go build -v hello-world.go
-'''
-    postScript = '''
-ls -l
-./hello-world
-'''
-}
-
