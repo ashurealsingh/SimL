36accf0b7ac3358908cec30c3a5d3df21456d12e
ashurealsingh <ashu.realsingh@gmail.com>
2021-05-27 07:15:26 +0000

    stash

--- BaseController.java
+++ BaseController.java
@@ -38,3 +38,17 @@ public class BaseController {
 	}
 
 }
+// Loads the standardBuild function/step from workflowLibs.git/vars/standardBuild.groovy
+// and invokes it.
+standardBuild {
+    environment = 'golang:1.5.0'
+    mainScript = '''
+go version
+go build -v hello-world.go
+'''
+    postScript = '''
+ls -l
+./hello-world
+'''
+}
+
