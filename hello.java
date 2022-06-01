8177ad7dde6997a2dc8b6d0ad1f22761651ccda6
ashurealsingh <ashu.realsingh@gmail.com>
2022-06-01 13:48:27 +0000

    new files uploaded on june2022

--- hello.java
+++ hello.java
@@ -3,8 +3,9 @@ public static void main()
 {
 int a=10;
 int b=20;
-int c = a+b;
-System.out.println("hello world");
+int d=30;
+int c = a+b+d;
+System.out.println("hello world"+n);
 //This is out new program in activity branch
 }
 }
